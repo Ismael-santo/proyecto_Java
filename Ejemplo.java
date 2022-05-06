5e6bb61b776870d946fb6dd48c2c35a3ca5c1f5e
Ismael-santo <elias.ismaelsantos10@gmail.com>
2022-05-06 13:41:22 -0600

    Ejemplo semana 6

--- Ejemplo.java
+++ Ejemplo.java
@@ -8,6 +8,12 @@ public class Ejemplo {
     public static void main(String[] args) {
         System.out.println("Este es una demostracion simplemente");
          //Declarar variables para aser una suma
+
+         int = (numero 1, numero 2);
+
+         //Formula
+         int suma= numero1 + numero2;
+         
          
     }
     
