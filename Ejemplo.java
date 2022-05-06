7274fb8d539655d556d8f40d2ef6316cd7281c2d
Ismael-santo <elias.ismaelsantos10@gmail.com>
2022-05-06 13:37:30 -0600

    Source Tree ejemplo Java

--- Ejemplo.java
+++ Ejemplo.java
@@ -6,6 +6,8 @@ public class Ejemplo {
 
     
     public static void main(String[] args) {
+        System.out.println("Este es una demostracion simplemente");
+         //Declarar variables para aser una suma
          
     }
     
